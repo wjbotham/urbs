7576ccb634e556080dabe7b5ad4413a49b06326b
Wesley Botham <wjbotham@gmail.com>
2015-10-12 07:37:12 -0500

    Add mysql2 dependency

--- bootstrap/install-gems.sh
+++ bootstrap/install-gems.sh
@@ -2,6 +2,10 @@
 
 # These are dependencies for the nokogiri gem.
 # TODO: Are libxslt-dev and libxml2-dev actually required with nokogiri 1.6+?
-apt-get install -y zlib1g-dev libxslt-dev libxml2-dev
+apt-get install -y zlib1g-dev libxslt-dev libxml2-dev 
+
+# Dependency for mysql2 gem
+apt-get install -y libmariadbclient-dev
+
 cd /vagrant
 bundle install
