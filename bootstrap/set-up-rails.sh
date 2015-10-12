24b77b9fa4c6bf083b2056dd401ab99da515bd2b
Wesley Botham <wjbotham@gmail.com>
2015-10-11 22:39:31 -0500

    Add install bootstrap for Ruby and MariaDB

--- bootstrap/set-up-rails.sh
+++ bootstrap/set-up-rails.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Rails setup script is still a TODO"
+#install ruby
+apt-get install -y ruby-full
+
+#install mariadb
+apt-get install -y python-software-properties
+
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/10.0/ubuntu precise main'
+
+apt-get update
+debconf-set-selections <<< "mysql-server mysql-server/root_password password MY_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password MY_PASSWORD"
+apt-get install -y mariadb-server
