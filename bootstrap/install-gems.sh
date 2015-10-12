6166f959ee2c8f78ad1f2d33cd3d056ef4229e98
Wesley Botham <wjbotham@gmail.com>
2015-10-12 01:18:08 -0500

    Correct context for 'bundle install'

--- bootstrap/install-gems.sh
+++ bootstrap/install-gems.sh
@@ -3,4 +3,5 @@
 # These are dependencies for the nokogiri gem.
 # TODO: Are libxslt-dev and libxml2-dev actually required with nokogiri 1.6+?
 apt-get install -y zlib1g-dev libxslt-dev libxml2-dev
+cd /vagrant
 bundle install
